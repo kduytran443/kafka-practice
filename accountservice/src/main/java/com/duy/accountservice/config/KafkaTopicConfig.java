c813c806cd8ba63f6c8c38202b9e9793ff0eac73
kduytran443 <trankhanhduy18@gmail.com>
2023-10-28 00:44:49 +0700

    #accountserivce: update topic value from properties file

--- accountservice/src/main/java/com/duy/accountservice/config/KafkaTopicConfig.java
+++ accountservice/src/main/java/com/duy/accountservice/config/KafkaTopicConfig.java
@@ -1,17 +1,24 @@
 package com.duy.accountservice.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KafkaTopicConfig {
 
+    @Value("${spring.kafka.topic.notification}")
+    private String notificationTopic;
+
+    @Value("${spring.kafka.topic.statistic}")
+    private String statisticTopic;
+
     NewTopic notification() {
-        return new NewTopic("notification", 2, (short) 1);
+        return new NewTopic(notificationTopic, 2, (short) 1);
     }
 
     NewTopic statistic() {
-        return new NewTopic("statistic", 1, (short) 1);
+        return new NewTopic(statisticTopic, 1, (short) 1);
     }
 
 }
